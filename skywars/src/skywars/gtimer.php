06571a297f8345df78a76c424bdbf9ddd3994ed2
soradore <soradore@svk.jp>
2017-08-17 00:12:25 +0900

    new

--- skywars/src/skywars/gtimer.php
+++ skywars/src/skywars/gtimer.php
@@ -7,20 +7,20 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use skywars\main;
 
-class gtimer extends PluginTask{
+class gtimer extends PluginTask
+{
+    public function __construct(PluginBase $owner)
+    {
+        parent::__construct($owner);
+    }
 
-          public function __construct(PluginBase $owner){
-                      parent::__construct($owner);
+    public function onRun($currentTick)
+    {
+        $this->getOwner()->timer();
+    }
 
-          }
-
-          public function onRun($currentTick){
-                      
-                      $this->getOwner()->timer();
-          }
-
-          public function stop(){
-
-                      $this->getOwner()->getServer()->getScheduler()->cancelTask($this->getTaskId());
-          }
+    public function stop()
+    {
+        $this->getOwner()->getServer()->getScheduler()->cancelTask($this->getTaskId());
+    }
 }   
